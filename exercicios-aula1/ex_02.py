e2c06a9dc9490ac40eecaae983e5499356f199b7
Jhonata Polito <jhonata.demuner@gmail.com>
2022-08-09 15:24:51 -0300

    Adicionando ex_02

---
+++ exercicios-aula1/ex_02.py
@@ -0,0 +1,9 @@
+# Faca um procedimento que leia 10 numeros digitados pelo usuaruio armazene a metade de cada um deles numa lista, e depois imprima esta lista.
+
+def metade():
+    lista = []
+    for i in range (10):
+        num = int(input())
+        lista.append(num / 2)
+
+    print(lista)
